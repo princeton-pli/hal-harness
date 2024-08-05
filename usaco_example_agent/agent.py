b29cd944444e3177e7eef6932e525cd34323722c
benediktstroebl <stroebl@princeton.edu>
2024-08-05 18:34:17 +0000

    new example agent format

---
+++ usaco_example_agent/agent.py
@@ -0,0 +1,20 @@
+import transformers
+import weave
+
+def run(input):
+    from openai import OpenAI
+    client = OpenAI()
+
+    with weave.attributes({'task_id': '1333_platinum_good_bitstrings'}):
+        response = client.chat.completions.create(
+            model="gpt-4o-mini-2024-07-18",
+            messages=[
+                {"role": "user", "content": 'test'},
+                ],
+            max_tokens=2000,
+            n=1,
+            temperature=1,
+        )
+    for task in input:
+        input[task]['response'] = 'test'
+    return input
