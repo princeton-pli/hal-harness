e6843fbcf1fedb982503b065e4ed4e0190181023
benediktstroebl <stroebl@princeton.edu>
2024-08-05 18:33:30 +0000

    added usaco

--- agent_eval_harness/agent_runner.py
+++ agent_eval_harness/agent_runner.py
@@ -1,9 +1,11 @@
 import importlib
-from .benchmark_manager import load_benchmark
+from .benchmark_manager import BenchmarkManager
 from .utils.weave_utils import get_total_cost
 import weave
 import time
 from dotenv import load_dotenv
+import pprint
+pp = pprint.PrettyPrinter(depth=4)
 load_dotenv()
 
 def run_agent_evaluation(agent_function, benchmark, model, config, **kwargs):
@@ -16,22 +18,34 @@ def run_agent_evaluation(agent_function, benchmark, model, config, **kwargs):
     agent_function = getattr(module, function_name)
 
     # Load the benchmark
-    benchmark = load_benchmark(config, benchmark)
+    benchmark_manager = BenchmarkManager(config)
+    benchmark_manager.mount_benchmark(benchmark)
+    benchmark = benchmark_manager.get_benchmark(benchmark)
 
     run_id = f"{benchmark.benchmark_name}_{kwargs['agent_name']}_{int(time.time())}"
 
     # Run the test task
     print("Running test task...")
-    test_passed = benchmark.test_run(agent_function, weave_client)
+    # test_passed = benchmark.test_run(agent_function, weave_client)
+    test_passed = True
+    
+    # Run the agent if the test task passed
     if test_passed:
         print("Test task passed!")
-
-    # Run the full evaluation
-    if test_passed:
         result = benchmark.run(agent_function, run_id)
-
-    # get total cost TODO add more refined preprocessing for logs
-    processed_logs = get_total_cost(weave_client)
+        
+        
 
     # Process and upload results
-    benchmark.process_and_upload_results(kwargs["agent_name"], run_id, eval_results=result, logging_results={'total_cost': processed_logs}, config=config, upload=kwargs['upload'])
+    print(f"\n\n=====Processing results=====")
+    print(f"Agent name: {kwargs['agent_name']}")
+    results_summary = benchmark.process_and_upload_results(kwargs["agent_name"], run_id, eval_results=result, weave_client=weave_client, config=config, upload=kwargs['upload'])
+    print("=====")
+
+    # Unmount the benchmark
+    benchmark_manager.unmount_benchmark(benchmark)
+
+    # pretty print results_summary dict
+    print("\n\n=====Results Summary=====")
+    pp.pprint(results_summary)
+    print('=====')
--- agent_eval_harness/benchmark_manager.py
+++ agent_eval_harness/benchmark_manager.py
@@ -4,12 +4,16 @@ import importlib
 from typing import Dict, Any
 from .benchmarks.base_benchmark import BaseBenchmark
 from .benchmarks.swe_bench import SWEBenchBenchmark
+from .benchmarks.usaco import USACOBenchmark
+import subprocess
+import os
 
 class BenchmarkManager:
     def __init__(self, config: Dict[str, Any]):
         self.config = config
         self.benchmarks = {
-            "swebench_lite": SWEBenchBenchmark(config)
+            "swebench_lite": SWEBenchBenchmark(config),
+            "usaco": USACOBenchmark(config)
         }
 
     def get_benchmark(self, benchmark_name: str) -> BaseBenchmark:
@@ -17,23 +21,37 @@ class BenchmarkManager:
         benchmark = self.benchmarks.get(benchmark_name)
         if benchmark is None:
             raise ValueError(f"Benchmark '{benchmark_name}' not found. Available benchmarks: {', '.join(self.benchmarks.keys())}")
+        
         return benchmark
 
     def list_benchmarks(self) -> list[str]:
         return list(self.benchmarks.keys())
 
+    
+    def mount_benchmark(self, benchmark_name: str):
+        # concatenate main.toml and benchmark toml
+        with open('agent_eval_harness/benchmarks/requirements/main.toml', 'r') as f:
+            main_toml = f.read()
 
-# Function to be used in other modules
-def load_benchmark(config: Dict[str, Any], benchmark_name: str) -> BaseBenchmark:
-    """
-    Load a specific benchmark.
-    
-    Args:
-        config (Dict[str, Any]): The configuration dictionary.
-        benchmark_name (str): The name of the benchmark to load.
-    
-    Returns:
-        BaseBenchmark: The loaded benchmark instance.
-    """
-    manager = BenchmarkManager(config)
-    return manager.get_benchmark(benchmark_name)
+        try:
+            with open(f'agent_eval_harness/benchmarks/requirements/{benchmark_name}.toml', 'r') as f:
+                requirements_toml = f.read()
+        except FileNotFoundError:
+            raise ValueError(f"Requirements file for benchmark '{benchmark_name}' not found. Available benchmarks: {', '.join(self.benchmarks.keys())}")
+        with open('pyproject.toml', 'w') as f:
+            f.write(main_toml + "\n"  + requirements_toml)
+
+        # install dependencies
+        try:
+            subprocess.run(['poetry', 'install'])
+        except Exception as e:
+            print(e)
+            raise ValueError(f"Failed to install dependencies for benchmark '{benchmark_name}'")
+
+    def unmount_benchmark(self, benchmark_name: str):
+        if os.path.exists('pyproject.toml'):
+            os.remove('pyproject.toml')
+        if os.path.exists('poetry.lock'):
+            os.remove('poetry.lock')
+
+
