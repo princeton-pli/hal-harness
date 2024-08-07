e0eb3f10dd1632a3e1d0b436409aca971c4f0907
benediktstroebl <stroebl@princeton.edu>
2024-08-07 22:24:25 +0000

    improved temp file for uploading to HF

--- agent_eval_harness/benchmarks/base_benchmark.py
+++ agent_eval_harness/benchmarks/base_benchmark.py
@@ -5,6 +5,7 @@ from typing import Dict
 import json
 import os
 from ..utils.weave_utils import get_total_cost, assert_task_id_logging
+import tempfile
 
 class BaseBenchmark(ABC):
 
@@ -41,28 +42,31 @@ class BaseBenchmark(ABC):
         except ValidationError as e:
             print("Agent output does not match expected format for output! Required output format" + str(e))
 
-    def validate_logging(self, weave_client, test_task_id):
+    def validate_logging(self, weave_client, test_weave_task_id):
         assert get_total_cost(weave_client) > 0, "Test run did not incur any cost"
 
-        assert_task_id_logging(weave_client, test_task_id)
+        assert_task_id_logging(weave_client, test_weave_task_id)
 
 
     def upload_results(self, run_id, upload_dict):
-        # Save results to a temp file
-        upload_path = f"results/{self.benchmark_name}/{run_id}_upload.json"
-        with open(upload_path, 'w') as f:
+        # Write results to temp file using tempfile
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
             json.dump(upload_dict, f)
+            temp_file_name = f.name
+            temp_file_path = os.path.abspath(temp_file_name)
+
         # Use huggingface API to upload results
         api = HfApi(token=os.getenv("HF_TOKEN", '.'))
         api.upload_file(
-            path_or_fileobj=upload_path,
+            path_or_fileobj=temp_file_path,
             path_in_repo=f"{run_id}.json",
             repo_id="agent-evals/results"   ,
             repo_type="dataset",
             commit_message=f"Add {run_id} to leaderboard.",
             )
         
-        # remove temp file
-        os.remove(upload_path)
+
+        # unlink the file
+        os.unlink(temp_file_name)
 
         print(f"Results uploaded to Hugging Face Hub.")
