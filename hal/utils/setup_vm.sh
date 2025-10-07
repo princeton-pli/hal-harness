853cf33793a6c50f83509309bae7baeaefa6d5ce
Kangheng Liu <72962885+kanghengliu@users.noreply.github.com>
2025-10-07 19:40:11 -0400

    fix(hal): weave gql incompatibility (#117)
    pin gql<4 to avoid weave issues

--- hal/utils/setup_vm.sh
+++ hal/utils/setup_vm.sh
@@ -67,10 +67,8 @@ su - $USERNAME -c "bash -c '\
     if [ -f requirements.txt ]; then \
         echo \"Installing requirements...\" && \
         pip install -r requirements.txt && \
-        echo \"Installing weave...\" && \
-        pip install weave==0.51.41 && \
-        echo \"Pinning gql and wandb for Weave compatibility...\" && \
-        pip install \"gql>=3.4,<4\" \"wandb>=0.17.0\" && \
+        echo \"Installing weave and gql pin...\" && \
+        pip install weave==0.51.41 \"gql<4\" && \
         echo \"Requirements installed\"; \
     else \
         echo \"No requirements.txt found\" && \
